434fef52a39973452f854aeb976ef713a5c31dae
logistics-crypto <logistics.aliouxx@gmail.com>
2023-03-29 21:37:18 -0700

    rede

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -16,11 +16,7 @@ int main(void)
     month = 4;
     day = 01;
     year = 1997;
-    day = convert_day(month, day);
     
     printf("Date: %02d/%02d/%04d\n", month, day, year);
-
-    print_remaining_days(month, day, year);
-
     return (0);
 }
